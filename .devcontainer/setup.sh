ece698cb91d55df99e52ff6ddc5d9fce1cc0d7e2
Rembrandt Kuipers <rem@kuipers.uk>
2025-04-03 20:20:12 +0000

    build(container): improving container setup

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -4,3 +4,13 @@ set -e
 cd "$(dirname "$0")/.."
 
 echo "Installing packages..."
+corepack enable
+
+curl -L https://foundry.paradigm.xyz | bash
+
+# Dynamically determine the home directory of the current user
+USER_HOME=$(eval echo ~"$USER")
+
+# Source the user's .bashrc and run foundryup
+# source "$USER_HOME/.bashrc"
+"$USER_HOME/.foundry/bin/foundryup"
